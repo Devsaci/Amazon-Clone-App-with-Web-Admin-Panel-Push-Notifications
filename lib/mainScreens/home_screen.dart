4279cacb6f76e386c67d82ab2221513a9cd9a235
saci zakaria <sacideveloppeur@gmail.com>
2023-04-14 16:00:27 +0200

    flexibleSpace: Container(),

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text("iShop"),
+        flexibleSpace: Container(),
       ),
     );
   }
