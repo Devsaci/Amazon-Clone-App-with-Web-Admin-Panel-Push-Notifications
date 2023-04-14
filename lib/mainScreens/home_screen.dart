bac03cd8abe7dc1ec23487719a719ca8bdf97201
saci zakaria <sacideveloppeur@gmail.com>
2023-04-14 16:19:47 +0200

    gradient: LinearGradient

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -15,7 +15,36 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         title: const Text("iShop"),
         flexibleSpace: Container(
-          decoration: const BoxDecoration(),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.pinkAccent,
+                Colors.purpleAccent,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          height: 400,
+          width: 200,
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [
+                Colors.yellow.shade50,
+                Colors.yellow.shade900,
+              ],
+              //begin: FractionalOffset(0.0, 0.0),
+              // end: FractionalOffset(1.0, 0.0),
+              // stops: [0.0, 1.0],
+              tileMode: TileMode.repeated,
+            ),
+          ),
         ),
       ),
     );
