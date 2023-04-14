bfc7554b14209af5389687265bbf6ffafa5269cc
saci zakaria <sacideveloppeur@gmail.com>
2023-04-14 15:58:39 +0200

    title: const Text("iShop"),

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -11,7 +11,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("iShop"),
+      ),
     );
   }
 }
